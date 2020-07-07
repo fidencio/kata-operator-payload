9d0b543a99d312bef58b0181d81b2e1bcdaa833b
Fabiano Fidêncio <fidencio@redhat.com>
2020-07-07 20:31:41 +0200

    travis: Integrate with Travis CI
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>

---
+++ hack/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -xe
+
+IMAGE_NAME="quay.io/fidencio/test"
+
+if "${TRAVIS_BRANCH}" == "master"; then
+	TAG="latest-${TRAVIS_CPU_ARCH}"
+else
+	TAG="${TRAVIS_BRANCH}-${TRAVIS_CPU_ARCH}"
+fi
+
+docker login \
+	--username="${QUAY_USERNAME}" \
+	--password="${QUAY_PASSWORD}" \
+	quay.io
+
+docker build \
+	--tag "${IMAGE_NAME}:${TAG}" \
+	--build-arg ARCH="${TRAVIS_CPU_ARCH}" \
+	.
+
+docker push "${IMAGE_NAME}:${TAG}"
